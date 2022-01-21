4cedf4945938232f4110f42331dc51a8bfb40b4d
Andrea <dalianamara@yahoo.com>
2022-01-21 19:24:19 +0200

    [LIB-4] Added login functionality

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,80 +1,118 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.css";
 import Footer from "../Footer";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState({});
+  const [records, setRecords] = useState([]);
+  const users = records.map((record) => record);
+  const errorMessages = {
+    uname: "invalid username",
+    pass: "invalid password",
+  };
+
   const valid = () => {
     return username.length > 0 && password.length > 0;
   };
+  useEffect(() => {
+    async function getRecords() {
+      const response = await fetch(`http://localhost:5000/record/`);
+      if (!response.ok) {
+        const message = `An error occured: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
 
-  const submit = async (e) => {
-    e.preventDefault();
-  };
+      const records = await response.json();
+      setRecords(records);
+    }
+    getRecords();
+    return;
+  }, [records.length]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    let { uname, pass } = document.forms[0];
+    const userdata = users.find((user) => user.username === uname.value);
+    if (userdata) {
+      if (userdata.password !== pass.value) {
+      } else {
+        console.log("wrong password");
+        setSuccess(true);
+      }
+    } else {
+      console.log("wrong username");
+    }
   };
 
-  return (
-    <>
-      <div className="content">
-        <center>
-          <br />
-          <h1 style={{ "margin-block-end": "0em" }}>Login</h1>
-          <hr
-            style={{ border: "1px solid black", borderColor: "#A04000" }}
-          ></hr>
-          <form onSubmit={handleSubmit}>
-            <label>Username</label>
+  const renderForm = () => {
+    return (
+      <>
+        <div className="content">
+          <center>
             <br />
-            <input
-              type="text"
-              id={"username"}
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <br />
-            <label>Password</label>
-            <br />
-            <input
-              id={"password"}
-              type="text"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <br />
-            <br />
-            <button
-              type="submit"
-              block
-              size="lg"
-              id={"submit"}
-              disabled={!valid()}
-              submit={submit}
-            >
-              Login
-            </button>
-            <br />
+            <h1 style={{ "margin-block-end": "0em" }}>Login</h1>
+            <hr
+              style={{ border: "1px solid black", borderColor: "#A04000" }}
+            ></hr>
+            <form onSubmit={handleSubmit}>
+              <label>Username</label>
+              <br />
+              <input
+                type="text"
+                name="uname"
+                required
+                id={"username"}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <br />
+              <label>Password</label>
+              <br />
+              <input
+                id={"password"}
+                name="pass"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <br />
+              <br />
+              <button
+                type="submit"
+                block
+                size="lg"
+                id={"submit"}
+                disabled={!valid()}
+                submit={handleSubmit}
+              >
+                Login
+              </button>
+              <br />
 
-            <Link
-              to="/signup"
-              style={{
-                color: "black",
-                marginLeft: "52%",
-                backgroundColor: "transparent",
-              }}
-            >
-              Don't have an account?
-            </Link>
-          </form>
-          <br /> <br />
-        </center>
-      </div>
-      <Footer></Footer>
-    </>
-  );
+              <Link
+                to="/signup"
+                style={{
+                  color: "black",
+                  marginLeft: "52%",
+                  backgroundColor: "transparent",
+                }}
+              >
+                Don't have an account?
+              </Link>
+            </form>
+            <br /> <br />
+          </center>
+        </div>
+        <Footer></Footer>
+      </>
+    );
+  };
+  return <div>{success ? <Navigate to="/services" /> : renderForm()}</div>;
 };
 
 export default Login;
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../Footer";
 import "../css/SignUp.css";
+import { render } from "@testing-library/react";
 const SignUp = () => {
   const [model, setModel] = useState({
     first: "",
@@ -9,6 +10,11 @@ const SignUp = () => {
     username: "",
     password: "",
   });
+
+  const [validPass, setValidPass] = useState(true);
+  const [validUname, setValidUname] = useState(true);
+  const [validEmail, setValidEmail] = useState(true);
+
   const update = (value) => {
     return setModel((prev) => {
       return { ...prev, ...value };
@@ -16,112 +22,185 @@ const SignUp = () => {
   };
   async function handleSubmit(e) {
     e.preventDefault();
-    const newUser = { ...model };
-    await fetch("http://localhost:5000/record/add", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newUser),
-    }).catch((error) => {
-      window.alert(error);
-      return;
-    });
-    setModel({ first: "", last: "", email: "", username: "", password: "" });
+    const { uname, pass, email } = document.forms[0];
+    const validPassword = validatePassword(pass);
+    const validUsername = validateUsername(uname);
+    const validEmailB = validateEmail(email);
+    console.log(validPassword, validEmailB, validUsername);
+    if (validPassword && validEmailB && validUsername === true) {
+      const newUser = { ...model };
+      await fetch("http://localhost:5000/record/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      }).catch((error) => {
+        window.alert(error);
+        return;
+      });
+      setModel({ first: "", last: "", email: "", username: "", password: "" });
+    }
   }
 
-  return (
-    <>
-      <div className="content">
-        <center>
-          <br />
-          <h1 style={{ "margin-block-end": "0em" }}>Sign up</h1>
-          <hr
-            style={{ border: "1px solid black", borderColor: "#A04000" }}
-          ></hr>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="firstname" style={{ float: "left" }}>
-                First name
-              </label>
-              <br />
-              <input
-                type="text"
-                className="form-control"
-                id="firstname"
-                value={model.first}
-                onChange={(e) => update({ first: e.target.value })}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="lastname" style={{ float: "left" }}>
-                Last name
-              </label>
-              <br />
-              <input
-                type="text"
-                className="form-control"
-                id="lastname"
-                value={model.last}
-                style={{ width: "100%" }}
-                onChange={(e) => update({ last: e.target.value })}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="username" style={{ float: "left" }}>
-                Username
-              </label>
-              <br />
-              <input
-                type="text"
-                className="form-control"
-                id="username"
-                value={model.username}
-                style={{ width: "100%" }}
-                onChange={(e) => update({ username: e.target.value })}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email" style={{ float: "left" }}>
-                Email
-              </label>
-              <br />
-              <input
-                type="text"
-                className="form-control"
-                id="email"
-                value={model.email}
-                onChange={(e) => update({ email: e.target.value })}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password" style={{ float: "left" }}>
-                Password
-              </label>
-              <br />
-              <input
-                type="text"
-                className="form-control"
-                id="password"
-                value={model.password}
-                style={{ width: "100%" }}
-                onChange={(e) => update({ password: e.target.value })}
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="submit"
-                value="Create user"
-                className="btn btn-primary"
-              />
-            </div>
-          </form>
-          <br />
-        </center>
-      </div>
-      <Footer></Footer>
-    </>
-  );
+  const validatePassword = (pass) => {
+    const password = /^[A-Za-z]\w{6,14}$/;
+    if (pass.value.match(password)) {
+      setValidPass(true);
+      return true;
+    } else {
+      setValidPass(false);
+
+      return false;
+    }
+  };
+
+  const validateEmail = (email) => {
+    const emailExpression = /\S+@\S+\.\S+/;
+    if (email.value.match(emailExpression)) {
+      setValidEmail(true);
+      return true;
+    } else {
+      setValidEmail(false);
+      return false;
+    }
+  };
+
+  const validateUsername = (uname) => {
+    const username = /^[A-Za-z]\w{5,30}$/;
+    if (uname.value.match(username)) {
+      setValidUname(true);
+      return true;
+    } else {
+      setValidUname(false);
+      return false;
+    }
+  };
+
+  const renderSignUpForm = () => {
+    return (
+      <>
+        <div className="content">
+          <center>
+            <br />
+            <h1 style={{ "margin-block-end": "0em" }}>Sign up</h1>
+            <hr
+              style={{ border: "1px solid black", borderColor: "#A04000" }}
+            ></hr>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="firstname" style={{ float: "left" }}>
+                  First name<span style={{ color: "red" }}>*</span>
+                </label>
+                <br />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="firstname"
+                  required
+                  value={model.first}
+                  onChange={(e) => update({ first: e.target.value })}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="lastname" style={{ float: "left" }}>
+                  Last name<span style={{ color: "red" }}>*</span>
+                </label>
+                <br />
+                <input
+                  type="text"
+                  required
+                  className="form-control"
+                  id="lastname"
+                  value={model.last}
+                  style={{ width: "100%" }}
+                  onChange={(e) => update({ last: e.target.value })}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="username" style={{ float: "left" }}>
+                  Username<span style={{ color: "red" }}>*</span>
+                </label>
+                <br />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  required
+                  name="uname"
+                  value={model.username}
+                  style={{ width: "100%" }}
+                  onChange={(e) => update({ username: e.target.value })}
+                />
+                {!validUname ? (
+                  <span className="error" style={{ color: "red" }}>
+                    eroare user
+                  </span>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" style={{ float: "left" }}>
+                  Email<span style={{ color: "red" }}>*</span>
+                </label>
+                <br />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  required
+                  name="email"
+                  value={model.email}
+                  onChange={(e) => update({ email: e.target.value })}
+                />
+                {!validEmail ? (
+                  <span className="error" style={{ color: "red" }}>
+                    eroare email
+                  </span>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" style={{ float: "left" }}>
+                  Password<span style={{ color: "red" }}>*</span>
+                </label>
+                <br />
+                <input
+                  type="text"
+                  className="form-control"
+                  id="password"
+                  name="pass"
+                  required
+                  value={model.password}
+                  style={{ width: "100%" }}
+                  onChange={(e) => update({ password: e.target.value })}
+                />
+                {!validPass ? (
+                  <span className="error" style={{ color: "red" }}>
+                    eroare password
+                  </span>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="form-group">
+                <input
+                  type="submit"
+                  value="Create user"
+                  className="btn btn-primary"
+                />
+              </div>
+            </form>
+            <br />
+          </center>
+        </div>
+        <Footer></Footer>
+      </>
+    );
+  };
+  return <div>{renderSignUpForm()}</div>;
 };
 
 export default SignUp;
